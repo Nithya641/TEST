6d598f11c82c32fbbe11689a28bc20f80a5e3aa3
Nithya641 <nithyashree.m@s.amity.edu>
2025-05-23 11:11:03 +0530

    added a code

---
+++ test.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+	printf("hello world");
+	printf("good morning")
+ printf("welcome");
+	printf("not well);
+	printf("cute")
+		printf("not well");
+	printf("very good")
+
+
